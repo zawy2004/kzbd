79130fba231d0dde1a5cc7f654874ed7dec560df
zawy2004 <161670490+zawy2004@users.noreply.github.com>
2024-07-11 17:21:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+
+var config = {
+   
+    texts: [
+        "Khánh Zân ơi",     
+        "Chúc kz",  
+        "Tuổi mới vui vẻ",
+        "Luôn Luôn hạnh phúc",
+        "đạt được nhiều thứ",
+        "đặc biệt",
+        "Phải thích đhuy nha",
+        "Đhuy sẽ bên cạnh kz hoi",
+        "nha nha",
+        "iuuuu ",
+        "hehe~~",
+    ],
+    
+    imgs: {
+        "KZ xinh qua": "./imgs/xiaokeai.png",
+        
+    },
+
+    desc: {
+        turn_on: "Mở",
+        play: "Bất đầu",
+        bannar_coming: "tiếp tục",
+        balloons_flying: "thiêú gì ấy nhỉ",
+        cake_fadein: "Bánh",
+        wish_message: "Happy birthday",
+        story: "A MESSAGE FOR Kz",
+    }
+};
